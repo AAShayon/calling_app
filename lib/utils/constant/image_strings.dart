81341aa0fad4c6d469f63fe3df75fe895899a39f
AAShayon <shayon.asif@gmail.com>
2024-02-28 15:16:17 +0600

    Image and Text Adding

---
+++ lib/utils/constant/image_strings.dart
@@ -0,0 +1,7 @@
+class AppImages{
+
+  static const String onBoardingImage1="assets/onBoarding/1.png";
+  static const String onBoardingImage2="assets/onBoarding/technology.png";
+  static const String onBoardingImage3="assets/onBoarding/voip.png";
+
+}
---
+++ lib/utils/constant/texts.dart
@@ -0,0 +1,10 @@
+class AppTexts{
+
+  static const pageTexts1='WellCome';
+  static const pageTexts2='Low rate phone call';
+  static const pageTexts3='Enjoy limitless Phone Call';
+  static const discription1='Test period 1';
+  static const discription2='Test period 2';
+  static const discription3='Test period 3';
+
+}
